403badbf2e67c1b2116c4738871871790cdd52ff
Jerry Wang <j3rry.wan9@gmail.com>
2021-09-15 00:26:39 -0700

    stitch everything together

---
+++ main.tf
@@ -0,0 +1,66 @@
+module "security_groups" {
+  source                  = "./modules/terraform-aws-security-groups"
+  server_port             = 80
+  container_port          = var.container_port
+  vpn_cidr_blocks         = var.vpn_cidr_blocks
+  alb_allowed_cidr_blocks = var.alb_allowed_cidr_blocks
+  vpc_id                  = data.aws_vpc.this.id
+}
+
+module "alb" {
+  source     = "./modules/terraform-aws-alb"
+  vpc_id     = data.aws_vpc.this.id
+  subnet_ids = data.aws_subnet_ids.default.ids
+  security_groups = [
+    module.security_groups.alb_security_group_id,
+  ]
+  container_port = var.container_port
+
+  depends_on = [
+    module.security_groups,
+  ]
+}
+
+module "asg" {
+  source                = "./modules/terraform-aws-asg"
+  image_id              = var.image_id
+  cw_demo_instance_type = var.cw_demo_instance_type
+  init_file             = data.template_file.init.rendered
+  vpc_security_group_ids = [
+    module.security_groups.asg_security_group_id,
+    module.security_groups.ssh_non_prod_vpn_security_group_id,
+  ]
+  subnet_ids = data.aws_subnet_ids.default.ids
+}
+
+module "ecs" {
+  source               = "./modules/terraform-aws-ecs"
+  ecs_cluster_name     = var.ecs_cluster_name
+  cw_demo_asg_arn      = module.asg.cw_demo_asg_arn
+  container_name       = var.container_name
+  container_port       = var.container_port
+  alb_target_group_arn = module.alb.alb_target_group_arn
+  sonar_jdbc_url       = module.rds.sonar_jdbc_url
+  sonar_jdbc_username  = var.sonar_jdbc_username
+  sonar_jdbc_password  = var.sonar_jdbc_password
+}
+
+module "rds" {
+  source                      = "./modules/terraform-aws-rds"
+  db_server_security_group_id = module.security_groups.db_server_security_group_id
+  vpc_id                      = data.aws_vpc.this.id
+  db_instance_name            = var.db_server_instance
+  db_instance_class           = var.db_server_instance_class
+  db_name                     = var.db_name
+  subnet_ids                  = data.aws_subnet_ids.default.ids
+  master_db_username          = var.master_db_username
+  master_db_password          = var.master_db_password
+  snapshot_identifier         = var.snapshot_identifier
+}
+
+module "sns" {
+  source           = "./modules/terraform-aws-sns"
+  account_id       = var.aws_account_id
+  recipient        = var.alert_email_address
+  cw_demo_asg_name = module.asg.cw_demo_asg_name
+}
--- variables.tf
+++ variables.tf
@@ -9,6 +9,7 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
 variable "aws_access_key" {
   type        = string
   description = "AWS access key"
@@ -17,4 +18,96 @@ variable "aws_access_key" {
 variable "aws_secret_key" {
   type        = string
   description = "AWS secret key"
+}
+
+variable "vpn_cidr_blocks" {
+  description = "VPN CIDR blocks"
+  type        = list(string)
+  default     = ["98.42.201.180/32"]
+}
+
+variable "alb_allowed_cidr_blocks" {
+  description = "CIDR blocks that are allowed to access ALB"
+  type        = list(string)
+  default     = ["98.42.201.180/32"]
+}
+
+variable "image_id" {
+  description = "AMI image ID"
+  type        = string
+  # Amazon Linux 2 ECS-optimized AMI in us-east-1
+  default = "ami-03db9b2aac6af477d"
+}
+
+variable "cw_demo_instance_type" {
+  type    = string
+  default = "t2.2xlarge"
+}
+
+variable "ecs_cluster_name" {
+  description = "ECS cluster name"
+  type        = string
+  default     = "cw-demo-cluster"
+}
+
+variable "container_name" {
+  description = "Docker container name"
+  type        = string
+  default     = "sonarqube"
+}
+
+variable "container_port" {
+  description = "Docker container port"
+  type        = number
+  default     = 9000
+}
+
+variable "alert_email_address" {
+  description = "The email address who will receive alerts"
+  type        = string
+}
+
+variable "db_server_instance" {
+  description = "The name of the DB instance"
+  type        = string
+  default     = "sonar"
+}
+
+variable "db_server_instance_class" {
+  description = "What size of DB instance to use, e.g. db.t3.medium"
+  type        = string
+  default     = "db.t3.medium"
+}
+
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+  default     = "sonarqubedb"
+}
+
+variable "master_db_username" {
+  description = "Username for the master DB user"
+  type        = string
+  default     = "postgres"
+}
+
+variable "master_db_password" {
+  description = "Password for the master DB user"
+}
+
+# If you wish to seed a DB from a backup, set the name of the snapshot here
+variable "snapshot_identifier" {
+  default = ""
+}
+
+variable "sonar_jdbc_username" {
+  description = "SonarQube DB JDBC username"
+  type        = string
+  default     = "sonar"
+}
+
+variable "sonar_jdbc_password" {
+  description = "SonarQube DB JDBC password"
+  type        = string
+  default     = "sonar"
 }
